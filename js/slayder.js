252158e54cba0e2f7f53aa506eca5cdaba1b6fe0
Liza Godun <godunelizaveta@ukr.net>
2018-09-23 20:57:10 +0300

    firstcommit

---
+++ js/slayder.js
@@ -0,0 +1,48 @@
+ window.onload=function(){
+
+var slayder,sl1_cl,sl2_cl,sl3_cl,sl4_cl,desc,num=1;
+slayder=document.getElementById("slayder");
+var cl=document.getElementsByClassName("cl");
+
+for (var i =0 ; i <= cl.length - 1; i++) {
+	cl[i].addEventListener("click", dot_click);
+}
+var timerId = setInterval(next, 3000);
+
+
+
+
+
+function dot_click(){
+
+if(num!=1)
+	{
+		slayder.classList.remove("sl_bg"+num);
+	}
+	var active=document.getElementsByClassName("circle_active");
+	active[0].classList.remove("circle_active");
+
+this.classList.add("circle_active");
+num=this.id;
+slayder.classList.add("sl_bg"+num);
+
+}
+function next(){
+
+	var dot=document.getElementById(num);//1
+	slayder.classList.remove("sl_bg"+num);
+	dot.classList.remove("circle_active");
+	num++;
+	console.log(num);
+	if (num>4) {
+	num=1;}
+
+
+	var dot_n=document.getElementById(num);
+	dot_n.classList.add("circle_active");
+	slayder.classList.add("sl_bg"+num);
+
+
+
+}
+ }
